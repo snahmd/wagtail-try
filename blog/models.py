adf6bb50979de79a5aff14630223e81a35f267dd
snahmd <sanahmed7@gmail.com>
2021-02-16 13:29:08 +0100

    adding blog listing and detail page with custom context

---
+++ blog/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField
+from streams import blocks
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+
+# Create your models here.
+
+class BlogListingPage(Page):
+
+    template = "blog/blog_listing_page.html"
+
+    custom_title = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        help_text= "Overwrites the default title",
+    )
+    content_panels = Page.content_panels + [
+        FieldPanel("custom_title"),
+        
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request,*args,**kwargs)
+        context["posts"] = BlogDetailPage.objects.live().public()
+        #context['extra'] = "Read all about it"
+        return context 
+
+class BlogDetailPage(Page):
+
+    custom_title = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        help_text= "Overwrites the default title",
+    )
+    blog_image = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=False,
+        null=True,
+        related_name = "+",
+        on_delete= models.SET_NULL,
+    )
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock()),
+            ("full_richtext", blocks.RichTextBlock()),
+            ("simple_richtext", blocks.SimpleRichTextBlock()),
+            ("cards", blocks.CardBlock()),
+            ("cta", blocks.CTABlock()),
+        ],
+        null= True,
+        blank = True
+    )
+    content_panels = Page.content_panels + [
+        FieldPanel("custom_title"),
+        ImageChooserPanel("blog_image"),
+        StreamFieldPanel("content"),
+    ]
+
